64b16ae1e036ba6680397994280ecba9a472a3b5
CosmicEon <sillimanite90@gmail.com>
2017-10-30 11:22:37 +0200

    fix port for socke.io

--- server/config/constants.js
+++ server/config/constants.js
@@ -1,15 +1,15 @@
 const config = {
     server: {
-        port: process.env.PORT || 8008, // process.env.PORT is used for heroku dynamic port generator
+        port: process.env.PORT || 8008,
     },
     mongo: {
-        url: 'mongodb://testuser:test1234@ds237475.mlab.com:37475/forum', // 'mongodb://localhost/forum'
+        url: 'mongodb://testuser:test1234@ds237475.mlab.com:37475/forum', // for local use -> 'mongodb://localhost/forum'
     },
     passport: {
         secret: 'NodeJS Secret Key',
     },
     socketIO: {
-        port: 8080, // default port for Socket.io?
+        port: process.env.PORT || 8080, // default port for Socket.io?
     },
 };
 
